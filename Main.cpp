3f53007f56773d6d6d2eb7fbd2d8da0f50063352
pingu111 <theo.carron@telecom-sudparis.eu>
2016-06-05 15:37:26 +0200

    Deplacement vers nouvelle scene OK

--- Main.cpp
+++ Main.cpp
@@ -1,49 +1,14 @@
 #include <memory>
 #include <iostream>
-#include <MenuPrincipal.h>
-#include <RythmNBlood.h>
-#include <WindowManager.h>
-#include <SFML/Graphics.hpp>
+#include <Global.h>
 
-enum SceneNames
-{
-	MENU,
-	RNB,
-	VILAIN,
-	WIN,
-	DEFEAT
-};
 
-static void moveToScene(SceneNames name , WindowManager* window)
-{
-	//TODO a faire avant de quitter une scene
-	switch (name)
-	{
-	case MENU:
-	{
-		MenuPrincipal scene(window);
-		break;
-	}
-	case RNB:
-	{
-		RythmNBlood scene(window);
-		break;
-	}
-	case VILAIN:
-		break;
-	case WIN:
-		break;
-	case DEFEAT:
-		break;
-	default:
-		break;
-	}
-	//TODO a faire au debut de toutes les scenes
-}
+
+
 
 int main()
 {
 	WindowManager windowManager;
-	moveToScene(SceneNames::MENU , &windowManager);
+	Global::moveToScene(SceneNames::MENU , &windowManager);
 	return 0;
 }
--- Sources/RythmNBlood.h
+++ Sources/RythmNBlood.h
@@ -1,10 +1,10 @@
 #pragma once
 
-#include <SFML/Graphics.hpp>
+#include <Global.h>
 #include <memory>
-#include <WindowManager.h>
 #include <VirtualScene.h>
 #include <Ennemi.h>
+
 
 
 class RythmNBlood : VirtualScene
@@ -13,9 +13,23 @@ class RythmNBlood : VirtualScene
 private:
 	std::vector<Ennemi> ennemis;
 	static Player player;
+
+	/* Lance la scene : appelé par le consytructeur */
 	void launchScene();
+
+	/* Charge les images */
 	void initTextures();
+
+	/* Met à jour la scene, les ennemis, ... */
 	void updateScene();
+
+	/* Affiche les images de fond et les boutons initiaux de la scene */
+	void printBackgroundAndButtons();
+
+
+	/* Les sprites et textures pour le background */
+	sf::Sprite fondSprite;
+	sf::Texture fond;
 
 public:
 	/* Constructeur
